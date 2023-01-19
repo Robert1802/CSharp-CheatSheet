72d6139e558b0705e90cb89faf2ae22a12d2e3c9
Roberto Moura <roblm_@hotmail.com>
2023-01-19 09:42:13 -0300

    Program

--- CSharp-CheatSheet/Program.cs
+++ CSharp-CheatSheet/Program.cs
@@ -9,11 +9,13 @@ namespace CheaSheet
             Console.WriteLine("Hello World!");
 
             /*
-             
+            
+            .NET:
             - .NET is a developer platform created by Microsoft which allows us to build or develop applications
             - .NET can build all sorts of applications: desktop apps, cloud apps, web apps, games, mobile apps and even AI
             - .NET can run everywhere (cross-platform), can run on windows, linux and mac
 
+            C#:
             - C# is the main language for .NET development
             - Object-oriented and type-safe programming language created by Microsoft that runs on the .NET Framework.
 
